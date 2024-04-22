c9aafe919cdcd6b3e8c12c2700dd30c9cfbe3716
paula.blancogonzalez <paula.blancogonzalez@paulablglez>
2024-04-22 16:51:57 +0200

    Triage pojo done

--- src/urgency/db/pojos/Triage.java
+++ src/urgency/db/pojos/Triage.java
@@ -1,5 +1,62 @@
 package urgency.db.pojos;
 
-public class Triage {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Triage implements Serializable {
+
+	private static final long serialVersionUID = -8137774737702387628L;
+	private Integer id;
+	private boolean available;
+	
+	public Triage () {
+		super();
+		this.available = true;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Triage (boolean available) {
+		super();
+		this.available = available;
+	}
+
+	@Override
+	public String toString() {
+		return "Triage [id=" + id + ", available=" + available + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Triage other = (Triage) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	
+	
 }
