45892927952a7a0e309f72cb4aefa8d6135c8ee8
paula.blancogonzalez <paula.blancogonzalez@paulablglez>
2024-04-26 12:09:42 +0200

    creates hechos

--- src/urgency/db/jdbc/ConnectionManager.java
+++ src/urgency/db/jdbc/ConnectionManager.java
@@ -4,6 +4,7 @@ import urgency.db.interfaces.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 
@@ -76,5 +77,102 @@ public class ConnectionManager {
 		ConnectionManager conMan = new ConnectionManager(); 
 		conMan.createConnection();
 		conMan.closeConnection();
+	}
+	
+	private void createTables() {
+		try {
+			// Create the tables
+			Statement createTables1 = connection.createStatement();
+			String createPatients = "CREATE TABLE Patients ( "
+					+ " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ " name TEXT NOT NULL,"
+					+ " surname TEXT NOT NULL"
+					+ " birthdate DATE NOT NULL,"
+					+ " sex TEXT NOT NULL,"
+					+ " weight INTEGER"
+					+ " height INTEGER"
+					+ " status TEXT NOT NULL CHECK (status = 'waiting' OR status = 'assisted' OR status = 'emergency room' OR status = 'discharged' OR status = 'hospitalized'),"
+					+ " urgency INTEGER CHECK (urgency = 1 OR ugency = 2 OR urgency = 3 OR urgency = 4 OR urgency = 5))";
+			createTables1.executeUpdate(createPatients);
+			createTables1.close();
+			
+			Statement createTables2 = connection.createStatement();
+			String createDoctors = "CREATE TABLE Doctors ( "
+					+ " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ " name TEXT NOT NULL,"
+					+ " surname TEXT NOT NULL,"
+					+ " speciality_type TEXT NOT NULL REFERENCES Speciality(type) ON DELETE RESTRICT,"
+					+ " in_box Boolean NOT NULL)";
+			createTables2.executeUpdate(createDoctors);
+			createTables2.close();
+			
+			Statement createTables3 = connection.createStatement();
+			String createSpecialities = "CREATE TABLE Specialities ( "
+					+ " type TEXT PRIMARY KEY)";
+			createTables3.executeUpdate(createSpecialities);
+			createTables3.close();
+			
+			Statement createTables4 = connection.createStatement();
+			String createTriages = "CREATE TABLE Triages ("
+					+ " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ " available Boolean NOT NULL)";
+			createTables4.executeUpdate(createTriages);
+			createTables4.close();
+	
+			Statement createTables5 = connection.createStatement();
+			String createBoxes = "CREATE TABLE Boxes ("
+					+ " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ " available Boolean NOT NULL,"
+					+ " speciality_type TEXT)";
+			createTables5.executeUpdate(createBoxes);
+			createTables5.close();
+			
+			Statement createTables6 = connection.createStatement();
+			String createPatientSpeciality = "CREATE TABLE PatientSpeciality ( "
+					+ " patient_id INTEGER REFERENCES Patients(id),"
+					+ " speciality_type TEXT REFERENCES Specialities(type),"
+					+ " date DATETIME NOT NULL,"
+					+ " PRIMARY KEY (patient_id, speciality_type))";
+			createTables6.executeUpdate(createPatientSpeciality);
+			createTables6.close();
+			
+			Statement createTables7 = connection.createStatement();
+			String createPatientTriage = "CREATE TABLE PatientTriage ( "
+					+ " patient_id INTEGER REFERENCES Patients(id),"
+					+ " triage_id INTEGER REFERENCES Triages(id),"
+					+ " date DATETIME NOT NULL,"
+					+ " PRIMARY KEY (patient_id, triage_id))";
+			createTables7.executeUpdate(createPatientTriage);
+			createTables7.close();
+			
+			Statement createTables8 = connection.createStatement();
+			String createPatientBox = "CREATE TABLE PatientBox ( "
+					+ " patient_id INTEGER REFERENCES Patients(id),"
+					+ " box_id INTEGER REFERENCES Boxes(id),"
+					+ " date DATETIME NOT NULL,"
+					+ " comments TEXT,"
+					+ " PRIMARY KEY (patient_id, box_id))";
+			createTables8.executeUpdate(createPatientBox);
+			createTables8.close();
+			
+			
+			Statement createTables9 = connection.createStatement();
+			String createBoxDoctor = "CREATE TABLE BoxDoctor ( "
+					+ " box_id INTEGER REFERENCES Boxes(id),"
+					+ " doctor_id INTEGER REFERENCES Doctors(id),"
+					+ " date DATETIME NOT NULL,"
+					+ " PRIMARY KEY (box_id, doctor_id))";
+			createTables9.executeUpdate(createBoxDoctor);
+			createTables9.close();
+			
+		} catch (SQLException sqlE) {
+			if (sqlE.getMessage().contains("already exist")){
+				System.out.println("No need to create the tables; already there");
+			}
+			else {
+				System.out.println("Error in query");
+				sqlE.printStackTrace();
+			}
+		}
 	}
 }
