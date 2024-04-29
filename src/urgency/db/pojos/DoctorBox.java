999aad1b7b6245bf4757b7d5c0eac4f5f2a76958
carme <carme@10.60.59.38>
2024-04-29 18:16:10 +0200

    .

--- src/urgency/db/pojos/DoctorBox.java
+++ src/urgency/db/pojos/DoctorBox.java
@@ -1,10 +1,37 @@
 package urgency.db.pojos;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class DoctorBox implements Serializable{
 	
-	private Doctor doctor;
-	private Box box;
+	private Integer id_doctor;
+	private Integer id_box;
+	
+	public DoctorBox() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id_box, id_doctor);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DoctorBox other = (DoctorBox) obj;
+		return Objects.equals(id_box, other.id_box) && Objects.equals(id_doctor, other.id_doctor);
+	}
+	
+	
+	
+	
+	
    
 }
--- src/urgency/db/pojos/PatientSpeciality.java
+++ src/urgency/db/pojos/PatientSpeciality.java
@@ -1,10 +1,34 @@
 package urgency.db.pojos;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class PatientSpeciality implements Serializable{
 	
-	private Patient patient;
-	private Speciality speciality;
+	private Integer id_patient;
+	private Integer id_speciality;
+	
+	public PatientSpeciality() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id_patient, id_speciality);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PatientSpeciality other = (PatientSpeciality) obj;
+		return Objects.equals(id_patient, other.id_patient) && Objects.equals(id_speciality, other.id_speciality);
+	}
+	
+	
 
 }
