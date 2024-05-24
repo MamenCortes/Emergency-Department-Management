e6b3156500a688b95218b4ae6460da282080c90d
paulablancog <paula.blancogonzalez@usp.ceu.es>
2024-05-24 16:29:10 +0200

    Merge branch 'main' of https://github.com/MamenCortes/Emergency-Department-Management into main
    xmlManager done

--- src/urgency/xml/utils/XmlManager.java
+++ src/urgency/xml/utils/XmlManager.java
@@ -3,24 +3,24 @@ package urgency.xml.utils;
 import java.io.File;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import urgency.db.jdbc.ConnectionManager;
 import urgency.db.pojos.Box;
 import urgency.db.pojos.Doctor;
 import urgency.db.pojos.Patient;
 
 public class XmlManager {
 	
-	private static EntityManager em;
+	private XmlManager XmlManager;
 	
 	public XmlManager() {
 		super();
+		this.XmlManager = XmlManager;
 	}
 
 	public Boolean Java2Xml (Doctor doctor) {
@@ -41,49 +41,21 @@ public class XmlManager {
 		}
 	}
 	
-	public Boolean Xml2Java (String pathway) {
-		JAXBContext jaxbContext = JAXBContext.newInstance(Doctor.class);
-		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-		
-		File file = new File(pathway);
-		Doctor doctor = (Doctor) unmarshaller.unmarshal(file);
-		System.out.println("Doctor:");
-		System.out.println("Name: " + doctor.getName());
-		System.out.println("Content: " + doctor.getSurname());
-		System.out.println("Date: " + doctor.getid());
-		List<Patient> patients = doctor.getPatients(); //we can also restore the report into the database
-		for (Patient patient : patients) {
-			System.out.println("Patient: " + patient.getName());
-		}
-		List<Box> boxes = doctor.getBoxes(); //we can also restore the report into the database
-		for (Box box : boxes) {
-			System.out.println("Boxes: " + box.getId());
-		}
-
-				// Store the report in the database
-				// Create entity manager
-				factory = Persistence.createEntityManagerFactory(PERSISTENCE_PROVIDER);
-				EntityManager em = factory.createEntityManager();
-				em.getTransaction().begin();
-				em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
-				em.getTransaction().commit();
-
-				// Create a transaction
-				EntityTransaction tx1 = em.getTransaction();
-
-				// Start transaction
-				tx1.begin();
-
-				// Persist
-				// We assume the authors are not already in the database
-				// In a real world, we should check if they already exist
-				// and update them instead of inserting as new
-				for (Employee employee : emps) {
-					em.persist(employee); //these employees cannot be already inside the database because if not they cannot be inserted
-				}
-				em.persist(report);
-				
-				// End transaction
-				tx1.commit();
+	public Doctor Xml2Java (String pathway) {
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(Doctor.class);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+			File file = new File(pathway);
+			if (!file.exists() || !file.isFile()) {
+				return null;
+			}else {
+				Doctor doctor = (Doctor) unmarshaller.unmarshal(file);
+				return doctor;
 			}
+			
+		} catch(JAXBException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
+}
